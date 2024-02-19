6085c35ae7ac8ed0af32a2845b6bf004f5faa091
Everton Renan <everton.renan@clevertech.biz>
2024-02-19 16:59:30 -0300

    feat: add navbar

---
+++ src/app/not-found.tsx
@@ -0,0 +1,17 @@
+const NotFound = () => {
+  return (
+    <div>
+      <main>
+        <div className='text-center'>
+          <h2>404 - Page Not Found</h2>
+          <p>
+            The page you are looking for might have been removed or temporarily
+            unavailable
+          </p>
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default NotFound;
